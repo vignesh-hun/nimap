e67bc129d20c11be123e6b4a1e550e1c20a28d9c
dravidan06 <88194380+dravidan06@users.noreply.github.com>
2021-08-06 18:08:28 +0530

    Delete node directory

--- node/routes/v1/category.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const router=express.Router()
-
-const controller = require('../../controller/category')
-
-router.post('/addCategory',controller.addCategory)
-router.get('/getCategory',controller.getCategory)
-
-module.exports = router
--- node/routes/v1/product.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const router=express.Router()
-
-const controller = require('../../controller/product')
-
-router.post('/addProduct',controller.addProduct)
-router.get('/getProduct',controller.getProduct)
-
-module.exports = router
